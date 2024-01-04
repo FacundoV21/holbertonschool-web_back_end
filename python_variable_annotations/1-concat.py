7782b7114b2cfd739afbcf7e27b6516a2d54f122
FacundoV21 <6756@holbertonstudents.com>
2024-01-04 16:07:47 +0000

    fixed task 0, added task 1 and 2

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""main function"""
+
+
+def add(str1: string, str2: string) -> string:
+    """Function to concat two strings"""
+    str3 = str1 + ' ' + str2
+    return str3
---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""main function"""
+
+
+def add(n: float) -> int:
+    """Function to concat two strings"""
+    return n//1
