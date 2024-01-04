aba5834b67cf0f87db878f65b1fe44abfe9110a2
FacundoV21 <6756@holbertonstudents.com>
2024-01-04 17:02:20 +0000

    fixed task 4, added tasks 5 and 6

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""main function"""
+
+
+def sum_list(input_list: List[float]) -> float():
+    """Function to return the string representation of a float"""
+    sum = 0
+    for x in input_list:
+        sum += x
+    return sum
---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""main function"""
+
+
+def sum_mixed_list(input_list: List[float, int]) -> float():
+    """Function to return the string representation of a float"""
+    sum = 0
+    for x in input_list:
+        sum += x
+    return sum
