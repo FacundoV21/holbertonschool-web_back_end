4de8fcdada37636c5283e9fbbf499946513c907d
FacundoV21 <6756@holbertonstudents.com>
2024-01-12 14:50:16 +0000

    fixed task 11

--- NoSQL/11-schools_by_topic.py
+++ NoSQL/11-schools_by_topic.py
@@ -5,7 +5,7 @@
 
 def schools_by_topic(mongo_collection, topic):
     """ lists schools by topic """
-    lista = mongo_collection.find({"topics": topics})
+    lista = mongo_collection.find({"topics": topic})
     if not lista:
         return []
     return lista
