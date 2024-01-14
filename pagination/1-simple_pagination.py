fce890721ec5c2d80de8a8e40262f17fe06542d3
FacundoV21 <6756@holbertonstudents.com>
2024-01-14 18:49:04 +0000

    added task 1

---
+++ pagination/1-simple_pagination.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+"""
+    return a tuple of size two containing a start index
+    and an end index corresponding to the range of indexes
+    to return in a list for those particular pagination parameters
+"""
+
+import csv
+import math
+from typing import List
+
+
+def index_range(page, page_size):
+    """returns the index and page"""
+    fPage = (page - 1) * page_size
+    return (fPage, fPage + page_size)
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+
+        assert(type(page)), (type(page_size)) == (int, int)
+        assert page > 0
+        assert page_size > 0
+
+        data = self.dataset()
+        index = index_range(page, page_size)
+
+        if len(data) > index[1]:
+            return data[index[0]:index[1]]
+
+        return []
