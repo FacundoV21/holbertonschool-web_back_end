3aed813dcbca17b8114d1a56f4a63bf288092e7d
FacundoV21 <6756@holbertonstudents.com>
2024-01-19 20:11:31 -0800

    fixed tasks 9 10 and 12

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,6 +1,6 @@
 export default function appendToEachArrayValue(array, appendString) {
   const arr2 = []
-  for (var idx in array) {
+  for (const idx in array) {
     arr2.push(appendString + idx)   ;
   }
 
