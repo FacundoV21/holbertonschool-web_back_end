af4defdf442d80b88eea827368c37cb74ee3e890
FacundoV21 <6756@holbertonstudents.com>
2024-01-28 04:48:49 -0800

    added tasks 1 and 2

---
+++ ES6_promise/1-promise.js
@@ -0,0 +1,12 @@
+import { error } from "console";
+import { reject } from "lodash";
+
+export default function getResponseFromAPI(success) {
+  return (new Promise((resolve) => { resolve('a');
+    if (success === true) {
+      resolve({ status: 200, body: 'Success'});
+    } else {
+      reject(new Error('The fake API is not working currently'))
+    } 
+  }));
+}
