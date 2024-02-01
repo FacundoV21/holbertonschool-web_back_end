5e8565d61c0530f9efc839dce0316c74a3bc3dd6
FacundoV21 <6756@holbertonstudents.com>
2024-02-01 09:01:17 -0800

    fixed task 5 and 7

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -4,6 +4,6 @@ export default function createInt8TypedArray(length, position, value) {
   }
   const buffer = new ArrayBuffer(length);
   const int8Array = new Int8Array(buffer);
-  int8Array[position] = value;  
+  int8Array[position] = value;
   return buffer;
 }
