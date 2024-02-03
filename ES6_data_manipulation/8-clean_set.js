d67f0087c8c2716b907194a562475b88def79c95
FacundoV21 <6756@holbertonstudents.com>
2024-02-03 16:35:04 +0000

    dixed task 8

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,5 @@
-function cleanSet(set, startString) {
-  const filteredValues = Array.from(set)
-    .filter(value => value.startsWith(startString))
-    .map(value => value.slice(startString.length));
-
-  return filteredValues.join('-');
+function cleanSet(inputSet, startString) {
+  const filteredValues = Array.from(inputSet).filter(value => value.startsWith(startString));
+  const resultString = filteredValues.map(value => value.substring(startString.length)).join('-');
+  return resultString;
 }
